9dadc34fc7637633e77ed75d86da14a6434c35b4
erifouD <artmandr@yandex.ru>
2022-11-11 00:05:25 +0300

    Если шо потом заберу

--- TestCpp/olymp.cpp
+++ TestCpp/olymp.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-bool unsigned_dots(vector<vector<int>> Matrix)
+bool unsigned_dots(vector<vector<int>> Matrix) //проверка на наличие изолированных вершин
 {
 	for (int i = 0; i != Matrix.size(); i++)
 		for (int j = 0; j != Matrix.size(); j++)
@@ -19,7 +19,7 @@ bool unique(vector<int> alpha, int value) //проверка на уникаль
 	}
 	return true;
 }
-vector<vector<int>> MakinNewPaths(vector<vector<int>> Mat, vector<int> CurPath, int fir) //создание последующих
+vector<vector<int>> MakinNewPaths(vector<vector<int>> Mat, vector<int> CurPath, int fir) //создание последующих точек в путях
 {
 	vector<vector<int>> Path;
 	for (int i = 0; i < Mat.size(); i++)
@@ -38,12 +38,8 @@ vector<vector<int>> CountPathsCheck(vector<vector<int>> Mat, int fir) //созд
 {
 	vector<vector<int>> Path;
 	for (int i = 0; i < Mat.size(); i++)
-	{
 		if (Mat[fir - 1][i])
-		{
 			Path.push_back({ Mat[fir - 1][i], fir, i + 1});
-		}
-	}
 	return Path;
 }
 int main()
@@ -57,7 +53,7 @@ int main()
 	vector<vector<int>> TempPaths; //неоконченные пути
 	vector<vector<int>> Paths; //завершенные пути
 
-	for (int i = 0; i < houses; i++) //создание матрицы
+	for (int i = 0; i < houses; i++) //создание матрицы смежности
 		for (int j = 0; j < houses; j++)
 			Matrix[i].push_back(0);
 
@@ -69,55 +65,53 @@ int main()
 		Matrix[h2 - 1][h1 - 1] = distance;
 	}
 
-	if (unsigned_dots(Matrix)) return 1; //проверка на неподведенные точки
+	if (unsigned_dots(Matrix)) return 1; //проверка на изолированные вершины. если есть программа обрывается
 	TempPaths = CountPathsCheck(Matrix, first); // присвоение первых путей
-
-		for (int i = 0; i < TempPaths.size(); i++)//цикл создания путей
+	for (int i = 0; i < TempPaths.size(); i++)//цикл создания путей
+	{
+		for (int seven = 0; seven < roads; seven++)//цикл с максимальной длиной пути если возможно
 		{
-			for (int seven = 0; seven < roads; seven++)//цикл с максимальной длиной пути если возможно
+			for (int give = 0; give < houses; give++) //цикл присвоение следующей конечной точки
 			{
-				for (int give = 0; give < houses; give++) //цикл присвоение следующей конечной точки
+				/*  если валидно значение в матрице текущей конечной точки пути и след. точки && значение нет в пути &&
+				↓ конечная точка не равна требуемой конечной (это завершенный путь и он не нуждается в повторном продлении)*/
+				if (Matrix[TempPaths[i].back() - 1][give] && unique(TempPaths[i], give + 1) && TempPaths[i].back() != last) 
 				{
-					if (Matrix[TempPaths[i].back() - 1][give] && unique(TempPaths[i], give + 1) && TempPaths[i].back() != last) //если валидно значение в матрице смежности 1- конечная точка в пути + след. конечная точка и такой точки не было
-					{
-						vector<vector<int>> Iter = MakinNewPaths(Matrix, TempPaths[i], TempPaths[i].back()); //временный вектор путей
-						TempPaths.erase(TempPaths.begin() + i); //удалить вектор, так как он будет множиться
-						for (int j = 0; j < Iter.size(); j++) //запись новых путей в вектор
-							TempPaths.push_back(Iter[j]);
-						i = 0;
-						break;
-					}
+					vector<vector<int>> Iter = MakinNewPaths(Matrix, TempPaths[i], TempPaths[i].back()); //временный вектор путей
+					TempPaths.erase(TempPaths.begin() + i); //удалить вектор, так как он будет множиться
+					for (int j = 0; j < Iter.size(); j++) //запись новых путей в вектор
+						TempPaths.push_back(Iter[j]);
+					i = 0;
+					break;
 				}
 			}
-			
-		}
-		for (int i = 0; i < TempPaths.size(); i++)
-			if (TempPaths[i].back() == last)
-				Paths.push_back(TempPaths[i]);
+		}	
+	}
+	for (int i = 0; i < TempPaths.size(); i++) //создание массива полных путей
+		if (TempPaths[i].back() == last)
+			Paths.push_back(TempPaths[i]); 
 
-		int min = Paths[0][0];
-		for (int i = 0; i < Paths.size(); i++)
-			if (Paths[i][0] < min) min = Paths[i][0];
+	int min = Paths[0][0];
+	for (int i = 0; i < Paths.size(); i++) //поиск минимальной длины пути
+		if (Paths[i][0] < min) min = Paths[i][0]; 
 
-		vector<vector<int>> ShortestPaths;
-		for (int i = 0; i < Paths.size(); i++)
-			if (Paths[i][0] == min)
-				ShortestPaths.push_back(Paths[i]);
+	vector<vector<int>> ShortestPaths;
+	for (int i = 0; i < Paths.size(); i++) //создание массива кратчайших путей
+		if (Paths[i][0] == min)
+			ShortestPaths.push_back(Paths[i]);
 
-		int maxlength = ShortestPaths[0].size();
-		for (int i = 0; i < ShortestPaths.size(); i++)
-			if (ShortestPaths[i].size() > maxlength) maxlength = ShortestPaths[i].size();
+	int maxlength = ShortestPaths[0].size(); //создание массива путей с максимальным кол-вом точек
+	for (int i = 0; i < ShortestPaths.size(); i++) 
+		if (ShortestPaths[i].size() > maxlength) maxlength = ShortestPaths[i].size();
 
-		for (int i = 0; i < ShortestPaths.size(); i++)
+	for (int i = 0; i < ShortestPaths.size(); i++)//вывод требуемых значений
+	{
+		if (ShortestPaths[i].size() == maxlength)
 		{
-			if (ShortestPaths[i].size() == maxlength)
-			{
-				cout << ShortestPaths[i][0] << endl;
-				for (int j = 2; j < ShortestPaths[i].size() - 1; j++)
-				{
-					cout << ShortestPaths[i][j] << " ";
-				}
-				return 0;
-			}
+			cout << ShortestPaths[i][0] << endl;
+			for (int j = 2; j < ShortestPaths[i].size() - 1; j++)
+				cout << ShortestPaths[i][j] << " ";
+			return 0;
 		}
+	}
 }
